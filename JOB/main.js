a8ac78ac0bead7b97f92954f605d01d51e4682c3
yanpaing007 <33965152+yanpaing007@users.noreply.github.com>
2023-06-29 05:43:55 +0630

    Add files via upload

---
+++ JOB/main.js
@@ -0,0 +1,8 @@
+function showme(){
+    var menu=document.getElementById("mynav");
+    if(menu.style.display!="block"){
+    menu.style.display="block";
+    }else{
+        menu.style.display="none";
+    }
+}
