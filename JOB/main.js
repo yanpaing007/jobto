c7d350cef2f07d31d628e99c003a2de7d857283f
yanpaing007 <33965152+yanpaing007@users.noreply.github.com>
2023-06-29 06:33:21 +0630

    Delete JOB directory

--- JOB/main.js
+++
@@ -1,8 +0,0 @@
-function showme(){
-    var menu=document.getElementById("mynav");
-    if(menu.style.display!="block"){
-    menu.style.display="block";
-    }else{
-        menu.style.display="none";
-    }
-}
